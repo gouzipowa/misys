ef9f9c37a0da63782378c9906d688f2981c83160
gouzipowa <armandguillon62@gmail.com>
2015-03-20 12:57:02 +0100

    base

---
+++ misys/src/main/java/com/mps/misys/MisysApplication.java
@@ -0,0 +1,12 @@
+package com.mps.misys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MisysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MisysApplication.class, args);
+    }
+}
---
+++ misys/src/test/java/com/mps/misys/MisysApplicationTests.java
@@ -0,0 +1,18 @@
+package com.mps.misys;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = MisysApplication.class)
+@WebAppConfiguration
+public class MisysApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
